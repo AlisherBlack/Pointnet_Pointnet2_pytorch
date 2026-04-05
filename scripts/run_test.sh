93b060e045799719c83a0b3110be54b81f34b7f7
Alisher Ibragimov <AlisherBlackMSU@yandex.ru>
2026-04-05 23:24:28 +0300

    fix test script

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -4,7 +4,10 @@ pointnet_dir=$(realpath $(dirname $0)/..)
 
 pushd $pointnet_dir >/dev/null
 
-python test_semseg.py --log_dir pointnet_sem_seg --test_area 5 --root_dir ./data/stanford_indoor3d-sanity-check/
+python test_semseg.py \
+    --log_dir pointnet_sem_seg_wo_stn \
+    --root_dir ./data/stanford_indoor3d/ \
+    --test_area 5 \
 
 popd >/dev/null
 
